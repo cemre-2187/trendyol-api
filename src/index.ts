f9e2befed45cab33cce1b179838a3c9db989ea05
cemre-2187 <55685979+cemre-2187@users.noreply.github.com>
2025-06-28 22:49:12 +0200

    add Order types export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
+export { Trendyol } from "./trendyol";
 
-export { Trendyol } from './trendyol'
+export { ProductApiResponse, Product } from "./interfaces/IProduct";
 
-export { ProductApiResponse, Product } from './interfaces/IProduct'
-
-export { FinancialApiResponse } from './interfaces/IFinancials'
-export { OrderApiResponse } from './interfaces/IOrder'
-export { Options } from './interfaces/IOptions'
-export { SettlementApiResponse } from './interfaces/ISettlements'
-export { InvoiceApiResponse, Invoice } from './interfaces/IInvoices'
+export { FinancialApiResponse } from "./interfaces/IFinancials";
+export { OrderApiResponse, Order, Line } from "./interfaces/IOrder";
+export { Options } from "./interfaces/IOptions";
+export { SettlementApiResponse } from "./interfaces/ISettlements";
+export { InvoiceApiResponse, Invoice } from "./interfaces/IInvoices";
 // Get Paymnet History
