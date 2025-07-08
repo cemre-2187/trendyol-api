a5c5e5dbf7bbe0cba26068df62ee425ae3256640
cemre-2187 <55685979+cemre-2187@users.noreply.github.com>
2025-07-08 14:28:53 +0200

    fix: user agent

--- src/methods/getProductDetailByBarcode.ts
+++ src/methods/getProductDetailByBarcode.ts
@@ -1,28 +1,40 @@
-import axios from "axios"
-import { ProductApiResponse } from "../interfaces/IProduct"
-import { Options } from "../interfaces/IOptions"
+import axios from "axios";
+import { ProductApiResponse } from "../interfaces/IProduct";
+import { Options } from "../interfaces/IOptions";
 
-export const getProductDetailByBarcodeMethod = async (shopId: number, apiKey: string, apiSecret: string, options: Options): Promise<ProductApiResponse> => {
-
+export const getProductDetailByBarcodeMethod = async (
+  shopId: number,
+  apiKey: string,
+  apiSecret: string,
+  options: Options
+): Promise<ProductApiResponse> => {
   if (!options.barcode) {
-    throw Error("Please provide barcode properly")
+    throw Error("Please provide barcode properly");
   }
 
-  const token: string = Buffer.from(`${apiKey}:${apiSecret}`, 'utf8').toString('base64')
+  const token: string = Buffer.from(`${apiKey}:${apiSecret}`, "utf8").toString(
+    "base64"
+  );
 
   let config: any = {
-    method: 'get',
+    method: "get",
     maxBodyLength: Infinity,
-    url: 'https://api.trendyol.com/sapigw/suppliers/' + shopId + '/products?page=0&size=50&approved=True&barcode=' + options.barcode,
+    url:
+      "https://api.trendyol.com/sapigw/suppliers/" +
+      shopId +
+      "/products?page=0&size=50&approved=True&barcode=" +
+      options.barcode,
     headers: {
-      'Authorization': 'Basic ' + token,
+      Authorization: "Basic " + token,
     },
-    maxRedirects: 0
+    maxRedirects: 0,
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "User-Agent": "Trendyol-API-Client",
   };
 
-  let response = await axios(config)
-  let result: ProductApiResponse
-  result = response.data
-  return result
-}
-
+  let response = await axios(config);
+  let result: ProductApiResponse;
+  result = response.data;
+  return result;
+};
